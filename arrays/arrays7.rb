377822f6625ed98c998a026f93204955656a2967
Harold Camacho <harold.camacho.diaz@gmail.com>
2021-09-01 10:28:38 -0400

    adding arrays

---
+++ arrays/arrays7.rb
@@ -0,0 +1,5 @@
+arr = [1, 2, 3]
+
+arr.each_with_index do |i,v|
+  puts "this #{i} is the value and this #{v} is the index"
+end   
---
+++ arrays/arrays8.rb
@@ -0,0 +1,9 @@
+arr1 = [1,2,3,4,5]
+arr2 = []
+
+arr2 = arr1.map do |i|
+  i + 2
+end
+
+p arr1
+p arr2     
